d38f1a3c83294bfcfa0d72f6e23db8ec418fca89
Guang Du <wolfee_tj@hotmail.com>
2017-07-19 10:34:28 +0800

    small enhance: LOG.info skipped kafka message, so ELK could pull from log and alert.

--- external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/KafkaSpout.java
+++ external/storm-kafka-client/src/main/java/org/apache/storm/kafka/spout/KafkaSpout.java
@@ -463,7 +463,7 @@ public class KafkaSpout<K, V> extends BaseRichSpout {
         }
         msgId.incrementNumFails();
         if (!retryService.schedule(msgId)) {
-            LOG.debug("Reached maximum number of retries. Message [{}] being marked as acked.", msgId);
+            LOG.info("Reached maximum number of retries. Message [{}] being marked as acked.", msgId);
             // this tuple should be removed from emitted only inside the ack() method. This is to ensure
             // that the OffsetManager for that TopicPartition is updated and allows commit progression
             ack(msgId);
